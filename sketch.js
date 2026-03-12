f7e7d4d5428d83b4291cadb5ef60b403814a38a6
pachudlt <pax.delatorre@gmail.com>
2026-03-11 22:25:35 -0300

    Update sketch.js
    update sketch with mobile interaction

--- sketch.js
+++ sketch.js
@@ -1,29 +1,22 @@
-let imagenes = [];
+let imgs = [];
 let imgActual = 0;
 
-let invertir = false;
-let glitch = false;
-let fragmentar = false;
-let pixelSorting = false;
+let colorShift = 0;
+let invertido = false;
 
-let colorIndex = 0;
-
-let paleta = [
-[255,255,255],
-[255,120,120],
-[120,255,180],
-[120,180,255],
-[255,220,120],
-[220,120,255]
-];
+let glitchActivo = false;
+let fragmentacionActiva = false;
+let pixelSortingActivo = false;
 
 let fragmentos = [];
 
+let esMobile = false;
+
 function preload(){
 
-imagenes[0] = loadImage("imagen_0.png");
-imagenes[1] = loadImage("imagen_1.png");
-imagenes[2] = loadImage("imagen_2.png");
+imgs[0] = loadImage("imagen_0.png");
+imgs[1] = loadImage("imagen_1.png");
+imgs[2] = loadImage("imagen_2.png");
 
 }
 
@@ -32,7 +25,19 @@ function setup(){
 createCanvas(windowWidth,windowHeight);
 imageMode(CENTER);
 
+esMobile = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
+
 crearFragmentos();
+
+if(esMobile){
+crearBotones();
+}
+
+}
+
+function windowResized(){
+
+resizeCanvas(windowWidth,windowHeight);
 
 }
 
@@ -40,91 +45,86 @@ function draw(){
 
 background(0);
 
-let img = imagenes[imgActual].get();
+let img = imgs[imgActual];
 
-if(invertir){
-img = invertirImagen(img);
-}
+push();
 
-if(pixelSorting){
-img = pixelSort(img);
-}
+translate(width/2,height/2);
 
 let escala = min(width/img.width,height/img.height);
+scale(escala);
 
-let w = img.width*escala;
-let h = img.height*escala;
+let temp = img.get();
 
-let c = paleta[colorIndex];
-tint(c[0],c[1],c[2]);
+if(colorShift>0){
 
-image(img,width/2,height/2,w,h);
+temp.loadPixels();
 
-noTint();
-
-if(glitch){
-efectoGlitch();
+for(let i=0;i<temp.pixels.length;i+=4){
+temp.pixels[i]+=colorShift;
 }
 
-if(fragmentar){
-fragmentacion(img,escala);
-}
+temp.updatePixels();
 
 }
 
-function invertirImagen(img){
+if(invertido){
+temp.filter(INVERT);
+}
 
-img.loadPixels();
+image(temp,0,0);
 
-for(let i=0;i<img.pixels.length;i+=4){
+pop();
 
-img.pixels[i]=255-img.pixels[i];
-img.pixels[i+1]=255-img.pixels[i+1];
-img.pixels[i+2]=255-img.pixels[i+2];
+if(glitchActivo) glitch();
+if(fragmentacionActiva) actualizarFragmentos();
+if(pixelSortingActivo) pixelSort();
 
 }
 
-img.updatePixels();
-return img;
+function keyPressed(){
+
+if(key==="0") imgActual=0;
+if(key==="1") imgActual=1;
+if(key==="2") imgActual=2;
+
+if(key==="C") colorShift=(colorShift+40)%200;
+
+if(key==="I") invertido=!invertido;
+
+if(key==="G") glitchActivo=!glitchActivo;
+
+if(key==="F") fragmentacionActiva=!fragmentacionActiva;
+
+if(key==="P") pixelSortingActivo=!pixelSortingActivo;
+
+if(key==="R") resetEfectos();
+
+if(key==="S") saveCanvas("captura","png");
 
 }
 
-function pixelSort(img){
+function resetEfectos(){
 
-img.loadPixels();
-
-for(let y=0;y<img.height;y+=6){
-
-let offset=int(random(-10,10));
-
-for(let x=0;x<img.width;x++){
-
-let i=(x+y*img.width)*4;
-let j=(constrain(x+offset,0,img.width-1)+y*img.width)*4;
-
-img.pixels[i]=img.pixels[j];
-img.pixels[i+1]=img.pixels[j+1];
-img.pixels[i+2]=img.pixels[j+2];
+colorShift=0;
+invertido=false;
+glitchActivo=false;
+fragmentacionActiva=false;
+pixelSortingActivo=false;
 
 }
 
-}
+function glitch(){
 
-img.updatePixels();
-return img;
-
-}
-
-function efectoGlitch(){
-
-for(let i=0;i<10;i++){
+for(let i=0;i<12;i++){
 
 let x=random(width);
 let y=random(height);
-let w=random(20,200);
-let h=random(5,40);
 
-copy(x,y,w,h,x+random(-60,60),y+random(-20,20),w,h);
+let w=random(40,140);
+let h=random(10,60);
+
+copy(x,y,w,h,x+random(-50,50),y);
 
 }
 
@@ -134,17 +134,9 @@ function crearFragmentos(){
 
 fragmentos=[];
 
-let img=imagenes[0];
-
-for(let i=0;i<35;i++){
+for(let i=0;i<80;i++){
 
 fragmentos.push({
-
-sx:random(img.width),
-sy:random(img.height),
-
-w:random(40,90),
-h:random(40,90),
 
 x:random(width),
 y:random(height),
@@ -152,8 +144,11 @@ y:random(height),
 vx:random(-0.4,0.4),
 vy:random(-0.4,0.4),
 
-angulo:random(TWO_PI),
-vRot:random(-0.01,0.01)
+rot:random(TWO_PI),
+vr:random(-0.01,0.01),
+
+w:random(60,120),
+h:random(60,120)
 
 });
 
@@ -161,58 +156,40 @@ vRot:random(-0.01,0.01)
 
 }
 
-function fragmentacion(img,escala){
+function actualizarFragmentos(){
+
+let img=imgs[imgActual];
 
 for(let f of fragmentos){
 
-// movimiento flotante
-f.vx += random(-0.02,0.02);
-f.vy += random(-0.02,0.02);
+let dx=f.x-mouseX;
+let dy=f.y-mouseY;
 
-f.vx = constrain(f.vx,-1,1);
-f.vy = constrain(f.vy,-1,1);
+let d=sqrt(dx*dx+dy*dy);
 
-// repulsion cursor
-let dx=mouseX-f.x;
-let dy=mouseY-f.y;
+if(d<120){
 
-let dist=sqrt(dx*dx+dy*dy);
-
-if(dist<160){
-
-f.vx-=dx*0.003;
-f.vy-=dy*0.003;
+f.vx+=dx*0.0006;
+f.vy+=dy*0.0006;
 
 }
 
-// actualizar posicion
 f.x+=f.vx;
 f.y+=f.vy;
 
-f.vx*=0.97;
-f.vy*=0.97;
+f.rot+=f.vr;
 
-// rebote bordes
-if(f.x<0||f.x>width)f.vx*=-1;
-if(f.y<0||f.y>height)f.vy*=-1;
-
-// rotacion
-f.angulo += f.vRot;
-
-// dibujar fragmento rotado
 push();
 
 translate(f.x,f.y);
-rotate(f.angulo);
+rotate(f.rot);
 
-copy(
-img,
-f.sx,f.sy,f.w,f.h,
--f.w*escala/2,
--f.h*escala/2,
-f.w*escala,
-f.h*escala
-);
+let sx=random(img.width);
+let sy=random(img.height);
+
+let frag=img.get(sx,sy,f.w,f.h);
+
+image(frag,0,0);
 
 pop();
 
@@ -220,52 +197,68 @@ pop();
 
 }
 
-function keyPressed(){
+function pixelSort(){
 
-if(key=='0') imgActual=0;
-if(key=='1') imgActual=1;
-if(key=='2') imgActual=2;
+loadPixels();
 
-if(key=='c'||key=='C'){
+for(let y=0;y<height;y+=4){
 
-colorIndex++;
+let x=int(random(width));
 
-if(colorIndex>=paleta.length){
-colorIndex=0;
-}
+let index=(x+y*width)*4;
+
+pixels[index]+=random(40);
 
 }
 
-if(key=='i'||key=='I') invertir=!invertir;
-
-if(key=='g'||key=='G') glitch=!glitch;
-
-if(key=='f'||key=='F') fragmentar=!fragmentar;
-
-if(key=='p'||key=='P') pixelSorting=!pixelSorting;
-
-if(key=='r'||key=='R'){
-
-invertir=false;
-glitch=false;
-fragmentar=false;
-pixelSorting=false;
-colorIndex=0;
-
-crearFragmentos();
+updatePixels();
 
 }
 
-if(key=='s'||key=='S'){
+function crearBotones(){
 
-saveCanvas("las_mil_y_una_imagenes","png");
+let controles=[
 
+["0",()=>imgActual=0],
+["1",()=>imgActual=1],
+["2",()=>imgActual=2],
+
+["C",()=>colorShift=(colorShift+40)%200],
+["I",()=>invertido=!invertido],
+
+["G",()=>glitchActivo=!glitchActivo],
+["F",()=>fragmentacionActiva=!fragmentacionActiva],
+["P",()=>pixelSortingActivo=!pixelSortingActivo],
+
+["R",()=>resetEfectos()],
+["S",()=>saveCanvas("captura","png")]
+
+];
+
+let x=10;
+let y=10;
+
+for(let c of controles){
+
+let btn=createButton(c[0]);
+
+btn.position(x,y);
+btn.size(44,44);
+
+btn.style("background","#000");
+btn.style("color","#fff");
+btn.style("border","1px solid white");
+btn.style("font-size","16px");
+
+btn.mousePressed(c[1]);
+
+y+=50;
+
+if(y>250){
+y=10;
+x+=50;
 }
 
 }
-
-function windowResized(){
-
-resizeCanvas(windowWidth,windowHeight);
 
 }
